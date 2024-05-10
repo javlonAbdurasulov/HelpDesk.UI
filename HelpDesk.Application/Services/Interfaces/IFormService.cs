73863fec6758190e9dc0cfb8a6329152dfa278aa
Javlon <abdurasulov2203@gmail.com>
2024-05-10 09:47:26 +0500

    interfaces create

---
+++ HelpDesk.Application/Services/Interfaces/IFormService.cs
@@ -0,0 +1,17 @@
+﻿using HelpDesk.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDesk.Application.Services.Interfaces
+{
+	public interface IFormService:
+		ICreateService<Forma>,
+		IUpdateService<Forma>,
+		IGetByIdService<Forma>
+	{
+
+	}
+}
---
+++ HelpDesk.Application/Services/Interfaces/ILetterService.cs
@@ -0,0 +1,19 @@
+﻿using HelpDesk.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDesk.Application.Services.Interfaces
+{
+	public interface ILetterService:
+		ICreateService<Letter>,
+		IUpdateService<Letter>,
+		IGetAllService<Letter>,
+		IGetByIdService<Letter>,
+		IDeleteService
+	{
+
+	}
+}
